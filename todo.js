c1842c660b606b627b1abb15d60ca1bca9e2d1c6
alto-I <blmaldiy@gmail.com>
2021-03-15 10:46:47 +0900

    todoアプリの実装

--- todo.js
+++ todo.js
@@ -1,1 +1,49 @@
-
+const vue = new Vue({
+  el: '#todo',
+  data: {
+    todolists: [],
+    newContent: null
+  },
+  mounted () {
+    if (localStorage.getItem('todolists')) {
+      try {
+        this.todolists = JSON.parse(localStorage.getItem('todolists'))
+      } catch (e) {
+        localStorage.removeItem('todolists')
+      }
+    }
+  },
+  methods: {
+    registration () {
+      if (!this.newContent) {
+        return
+      }
+      const todo = { content: this.newContent, checked: false }
+      this.todolists.push(todo)
+      this.newContent = ''
+      this.save()
+    },
+    change (index) {
+      if (this.todolists[index].checked) {
+        this.todolists[index].checked = false
+      } else {
+        this.todolists[index].checked = true
+      }
+      this.save()
+    },
+    remove (index) {
+      this.todolists.splice(index, 1)
+      this.save()
+    },
+    save () {
+      const parsed = JSON.stringify(this.todolists)
+      localStorage.setItem('todolists', parsed)
+    },
+    completedStyle (index) {
+      return {
+        opacity: this.todolists[index].checked ? 0.3 : 1.0,
+        'text-decoration': this.todolists[index].checked ? 'line-through' : ''
+      }
+    }
+  }
+})
